ff14d4b20daff3fcb2ee1c3c05275a1c79a03799
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-05 21:10:03 -0600

    main not complete

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -7,4 +7,5 @@ cash_coupon = int( input('Please enter cash coupon amount: '))
 if cash_coupon > 0:
     total_price_cash_coupon = price - cash_coupon
     print('Your total is $%0d' % (total_price_cash_coupon))
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    calculate_price()
