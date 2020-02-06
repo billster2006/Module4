da6cb0b3df598e6452d474c2d046a682e7984506
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-05 20:25:45 -0600

    Test not complete

---
+++ Test/test_coupon_calculations.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+def test_price_under_10():
+    price == 9
+    case_coupon == 5
+    percent_coupon == .10
+
+
+
+class MyTestCase(unittest.TestCase):
+    self.assertTrue()
+
+
+if __name__ == '__main__':
+    unittest.main()
