df3f191652ef5682a1916d291769d7903db275e0
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-11 21:10:09 -0600

    Test working

--- Test/test_coupon_calculations.py
+++ Test/test_coupon_calculations.py
@@ -13,14 +13,14 @@ class MyTestCase(unittest.TestCase):
         # price: under 10, 5 cash, 20%
         self.assertEqual(coupons.calculate_price(under_ten, 5.00, 20), 1.60)
         # price: under 10, 10 cash, 10%
-        self.assertEqual(coupons.calculate_price(under_ten, 10.00, 10), 0)
+        self.assertEqual(coupons.calculate_price(under_ten, 10.00, 10), 0.00)
 
     def test_price_under_between_ten_thirty(self):
         between_ten_thirty = 15
         # price: 10 to up to 30, 15, 5 cash, 10%
-        self.assertEqual(coupons.calculate_price(between_ten_thirty, 5.00, 10), 9)
+        self.assertEqual(coupons.calculate_price(between_ten_thirty, 5.00, 10), 9.00)
         # price: 10 to up to 30, 20, 5 cash, 20%
-        self.assertEqual(coupons.calculate_price(between_ten_thirty + 5, 5.00, 20), 12)
+        self.assertEqual(coupons.calculate_price(between_ten_thirty + 5, 5.00, 20), 12.00)
         # price: 10 to up to 30, 28, 10 cash, 10%
         self.assertEqual(coupons.calculate_price(between_ten_thirty + 13, 10.00, 10), 16.20)
 
@@ -29,8 +29,17 @@ class MyTestCase(unittest.TestCase):
 
         # price: 30 to up to 50, 34, 5 cash, 10%
         self.assertEqual(coupons.calculate_price(between_thirty_fifty, 5.00, 10), 26.10)
-        # price: 30 to up to 50, 32,  5 cash, 10%
+        # price: 30 to up to 50, 32,  10 cash, 20%
         self.assertEqual(coupons.calculate_price(between_thirty_fifty - 2, 10.00, 20), 17.60)
+        # price: 30 to up to 50, 45,  5 cash, 15%
+        self.assertEqual(coupons.calculate_price(between_thirty_fifty + 11, 5.00, 15), 34.00)
+
+    def test_price_under_over_fifty(self):
+        over_fifty = 65
+
+        # price: 50 and above, 5 cash, 10%
+        self.assertEqual(coupons.calculate_price(over_fifty, 5.00, 10), 54)
+
 
 
 if __name__ == '__main__':
