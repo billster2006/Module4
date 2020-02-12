f4959078fc92bde1a35796d89132c38166ff0f5b
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-11 18:42:36 -0600

    main and test not complete

--- Test/test_coupon_calculations.py
+++ Test/test_coupon_calculations.py
@@ -1,20 +1,15 @@
 import unittest
-import unittest.mock as mock
-from store import coupon_calculations
+#import unittest.mock as mock
+from store import coupon_calculations as coupons
 
 class MyTestCase(unittest.TestCase):
 
-    def test_price_under_10(self):
-        with mock.patch('builtins.input'):
-            if cash_coupon > 0:
-                total_price_cash_coupon = price - cash_coupon
-                if percent_coupon > 0 < cash_coupon:
-
-
-
-        self.assertTrue()
-
-
+    def test_price_under_ten(self):
+        dollar_amt = 7.00
+        assert coupons.calculate_price(dollar_amt, 5, 10) == 1.80
+        assert coupons.calculate_price(dollar_amt, 5, 15) == 1.70
+        assert coupons.calculate_price(dollar_amt, 5, 20) == 1.60
+        assert coupons.calculate_price(dollar_amt, 10, 10) == 0
 
 
 
--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -3,9 +3,21 @@ def calculate_price(price, cash_coupon, percent_coupon):
 
 price = int( input('Please enter price: '))
 cash_coupon = int( input('Please enter cash coupon amount: '))
+percent_coupon = float( input('Please enter coupon percent: '))
 
-if cash_coupon > 0:
-    total_price_cash_coupon = price - cash_coupon
-    print('Your total is $%0d' % (total_price_cash_coupon))
+if price < 10:
+    if cash_coupon < 10:
+        if percent_coupon < 20:
+
+
+
+
+        total_price_cash_coupon = price - cash_coupon
+        #print('Your total is $%0d' % (total_price_cash_coupon))
+
+
+
+
 if __name__ == '__main__':
     calculate_price()
+
